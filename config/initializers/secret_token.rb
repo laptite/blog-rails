67a553f14986353cefa84d30bb9b3be7f1c8293e
laptite <laptite@gmail.com>
2013-08-08 17:24:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogRails::Application.config.secret_token = '8a12d0a641e27f7b6782d94ef17e30ad3d9067e9ffd5169cf30e644ce4a7d9777de29afda2def295067c96e028ec0e974633b2363c6ef5d1432ed4cfa8d7b261'
